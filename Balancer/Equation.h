814506b08716bb42701681dfe6862693069419c8
yporteous <ghazalla>
2016-04-28 14:01:59 +0100

    First Commit, added Eqn class

---
+++ Balancer/Equation.h
@@ -0,0 +1,13 @@
+//
+//  Equation.h
+//  Balancer
+//
+//  Created by Younus Porteous on 2016/04/28.
+//  Copyright © 2016 Younus Porteous. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Equation : NSObject
+
+@end
---
+++ Balancer/Equation.m
@@ -0,0 +1,13 @@
+//
+//  Equation.m
+//  Balancer
+//
+//  Created by Younus Porteous on 2016/04/28.
+//  Copyright © 2016 Younus Porteous. All rights reserved.
+//
+
+#import "Equation.h"
+
+@implementation Equation
+
+@end
